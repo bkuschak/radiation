a990d0d6278659a6a0ded23c54b87c72d90967ef
root <root@beaglebone.localdomain>
2018-08-11 18:11:43 +0000

    Added weekly and monthly plots.  Fix plots to display full width.

--- counter/run.sh
+++ counter/run.sh
@@ -5,11 +5,13 @@
 # prerequisites:
 # sudo apt-get install ncftp gnuplot alsa-utils
 
-UPDATE_PERIOD_SEC=300
+UPDATE_PERIOD_SEC=600
 LOCATION="Sunnyvale, CA"
 DETECTOR_TYPE="Scionix 38B57/1.5M PMT + 38x38mm NaI(Tl)"
 
-# Make a gnuplot command file
+# Make gnuplot command files:
+
+# Daily
 cat << EOF > plot.gp
 set term png font arial 12 size 1280,640
 set output 'latest_radiation.png'
@@ -18,12 +20,49 @@ set xdata time
 set timefmt '%s'
 set format x "%H:%M\n%m-%d"
 set grid 
+set autoscale xfix
 
 set title "Radiation Level in $LOCATION\n$DETECTOR_TYPE"
 set ylabel 'Counts / min'
 set xlabel 'UTC Time'
 
 plot 'latest.log' using 1:3 with lines title 'Detector #1' lw 2
+EOF
+
+# Weekly
+cat << EOF > plot_weekly.gp
+set term png font arial 12 size 1280,640
+set output 'weekly_radiation.png'
+
+set xdata time
+set timefmt '%s'
+set format x "%H:%M\n%m-%d"
+set grid 
+set autoscale xfix
+
+set title "Weekly Radiation Level in $LOCATION\n$DETECTOR_TYPE"
+set ylabel 'Counts / min'
+set xlabel 'UTC Time'
+
+plot 'weekly.log' using 1:3 with lines title 'Detector #1' lw 2
+EOF
+
+# Monthly
+cat << EOF > plot_monthly.gp
+set term png font arial 12 size 1280,640
+set output 'monthly_radiation.png'
+
+set xdata time
+set timefmt '%s'
+set format x "%H:%M\n%m-%d"
+set grid 
+set autoscale xfix
+
+set title "Monthly Radiation Level in $LOCATION\n$DETECTOR_TYPE"
+set ylabel 'Counts / min'
+set xlabel 'UTC Time'
+
+plot 'monthly.log' using 1:3 with lines title 'Detector #1' lw 2
 EOF
 
 # Continuously capture and log
@@ -34,15 +73,25 @@ nice --1 arecord -f S16_LE -c 1 -r 48000  -t raw -D hw:1,0 | ./counter >> log.tx
 while true; do 
 	# Assuming file is logged at constant rate, compute the period
 	PERIOD=$(tail -2 log.txt  |awk ' {t[NR] = $1} END {print t[NR]-t[NR-1]}')
-	LINES=$((86400/$PERIOD))
+	LINES_DAY=$((86400/$PERIOD))
+	LINES_WEEK=$((7*86400/$PERIOD))
+	LINES_MONTH=$((30*86400/$PERIOD))
 
-	# Get one days' worth of lines
-	tail -$LINES log.txt > latest.log
+	# Get one day worth of lines
+	tail -$LINES_DAY log.txt > latest.log
 	gnuplot plot.gp
 
-	# upload plot to FTP server
+	# Get one week worth of lines
+	tail -$LINES_WEEK log.txt > weekly.log
+	gnuplot plot_weekly.gp
+
+	# Get one month worth of lines
+	tail -$LINES_MONTH log.txt > monthly.log
+	gnuplot plot_monthly.gp
+
+	# upload plots to FTP server
 	DIR=$(grep ^directory login.txt |sed 's?directory ??g')
-	ncftpput -f login.txt $DIR latest_radiation.png
+	ncftpput -f login.txt $DIR latest_radiation.png weekly_radiation.png monthly_radiation.png
 
 	sleep $UPDATE_PERIOD_SEC
 
